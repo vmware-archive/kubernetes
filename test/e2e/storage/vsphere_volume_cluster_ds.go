61b54a09f09910e8f01524fd530f562d0b8ab045
pshahzeb <pshahzeb@vmware.com>
2017-10-03 15:38:14 -0700

    Test to verify volume provisiong on a clustered datastore

---
+++ test/e2e/storage/vsphere_volume_cluster_ds.go
@@ -0,0 +1,111 @@
+package storage
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/pkg/cloudprovider/providers/vsphere"
+	"k8s.io/kubernetes/pkg/cloudprovider/providers/vsphere/vclib"
+	"k8s.io/kubernetes/test/e2e/framework"
+)
+
+/*
+	Tests to verify volume provisioning on a clustered datastore
+	1. Static provisioning
+	2. Dynamic provisioning
+	3. Dynamic provisioning with spbm policy
+
+	This test reads env
+	1. CLUSTER_DATASTORE which should be set to clustered datastore
+	2. VSPHERE_SPBM_POLICY_DS_CLUSTER which should be set to a tag based spbm policy tagged to a clustered datastore
+*/
+var _ = SIGDescribe("Volume Provision On Clustered Datastore", func() {
+	f := framework.NewDefaultFramework("volume-provision")
+
+	var client clientset.Interface
+	var namespace string
+	clusterDatastore := os.Getenv("CLUSTER_DATASTORE")
+	Expect(clusterDatastore).NotTo(BeEmpty())
+	var scParameters map[string]string
+
+	BeforeEach(func() {
+		framework.SkipUnlessProviderIs("vsphere")
+		client = f.ClientSet
+		namespace = f.Namespace.Name
+		scParameters = make(map[string]string)
+	})
+
+	/*
+		Steps:
+		1. Create volume options with datastore to be a clustered datastore
+		2. Create a vsphere volume
+		3. Create podspec with volume path. Create a corresponding pod
+		4. Verify disk is attached
+		5. Delete the pod and wait for the disk to be detached
+	*/
+
+	It("verify static provision", func() {
+		var volumePath string
+		vsp, err := vsphere.GetVSphere()
+		Expect(err).NotTo(HaveOccurred())
+
+		By("creating a test vsphere volume")
+		volumeOptions := new(vclib.VolumeOptions)
+		volumeOptions.CapacityKB = 2097152
+		volumeOptions.Name = "e2e-vmdk-" + strconv.FormatInt(time.Now().UnixNano(), 10)
+		volumeOptions.Datastore = clusterDatastore
+
+		volumePath, err = createVSphereVolume(vsp, volumeOptions)
+		Expect(err).NotTo(HaveOccurred())
+
+		podspec := getVSpherePodSpecWithVolumePaths([]string{volumePath}, nil, nil)
+
+		pod, err := client.CoreV1().Pods(namespace).Create(podspec)
+		Expect(err).NotTo(HaveOccurred())
+		By("Waiting for pod to be ready")
+		Expect(framework.WaitForPodNameRunningInNamespace(client, pod.Name, namespace)).To(Succeed())
+
+		// get fresh pod info
+		pod, err = client.CoreV1().Pods(namespace).Get(pod.Name, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		nodeName := types.NodeName(pod.Spec.NodeName)
+
+		isAttached, err := verifyVSphereDiskAttached(vsp, volumePath, types.NodeName(nodeName))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(isAttached).To(BeTrue(), "disk:"+volumePath+" is not attached with the node")
+
+		By("Deleting pod")
+		framework.DeletePodWithWait(f, client, pod)
+
+		By("Waiting for volumes to be detached from the node")
+		waitForVSphereDiskToDetach(vsp, volumePath, types.NodeName(nodeName))
+	})
+
+	/*
+		Steps:
+		1. Create storage class parameter and specify datastore to be a clustered datastore name
+		2. invokeValidPolicyTest - util to do e2e dynamic provision test
+	*/
+	It("verify dynamic provision with default parameter", func() {
+		scParameters[Datastore] = clusterDatastore
+		invokeValidPolicyTest(f, client, namespace, scParameters)
+	})
+
+	/*
+		Steps:
+		1. Create storage class parameter and specify storage policy to be a tag based spbm policy
+		2. invokeValidPolicyTest - util to do e2e dynamic provision test
+	*/
+	It("verify dynamic provision with spbm policy", func() {
+		goldPolicy := os.Getenv("VSPHERE_SPBM_POLICY_DS_CLUSTER")
+		Expect(goldPolicy).NotTo(BeEmpty())
+		scParameters[SpbmStoragePolicy] = goldPolicy
+		invokeValidPolicyTest(f, client, namespace, scParameters)
+	})
+})
